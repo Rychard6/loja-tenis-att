4932ebf9b872c56a75ca96441d362962b57ae61f
Rychard Ryan <rychardryan6@gmail.com>
2023-09-25 12:59:55 -0300

    new banner

--- src/components/header/navBar.js
+++ src/components/header/navBar.js
@@ -43,7 +43,7 @@ export default function NavBar(){
 
             {/* banner */}
             <div className='flex justify'>
-            <img src={imageUrl} alt="banner" className="w-16 h-10"/>
+                <img className="sm:ml-60 sm:mx-auto btn btn-ghost normal-case text-xl" src={imageUrl} alt="Banner" />
             </div>
 
             {/* buscar e darkMode */}
